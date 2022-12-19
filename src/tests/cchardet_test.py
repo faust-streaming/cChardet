01f8651955b4eb256cf111d77104f6daeb3729cd
William Barnhart <william.barnhart@he360.com>
2022-12-19 12:04:32 -0500

    replace test withs os.path.join to be OS agnostic and reenable Windows

--- src/tests/cchardet_test.py
+++ src/tests/cchardet_test.py
@@ -4,17 +4,17 @@ import os
 import cchardet
 
 SKIP_LIST = [
-    'src/tests/testdata/ja/utf-16le.txt',
-    'src/tests/testdata/ja/utf-16be.txt',
-    'src/tests/testdata/es/iso-8859-15.txt',
-    'src/tests/testdata/da/iso-8859-1.txt',
-    'src/tests/testdata/he/iso-8859-8.txt',
+    os.path.join('src','tests','testdata','ja','utf-16le.txt'),
+    os.path.join('src','tests','testdata','ja','utf-16be.txt'),
+    os.path.join('src','tests','testdata','es','iso-8859-15.txt'),
+    os.path.join('src','tests','testdata','da','iso-8859-1.txt'),
+    os.path.join('src','tests','testdata','he','iso-8859-8.txt'),
 ]
 
 # Python can't decode encoding
 SKIP_LIST_02 = [
-    'src/tests/testdata/vi/viscii.txt',
-    'src/tests/testdata/zh/euc-tw.txt',
+    os.path.join('src','tests','testdata','vi','viscii.txt'),
+    os.path.join('src','tests','testdata','zh','euc-tw.txt'),
 ]
 SKIP_LIST_02.extend(SKIP_LIST)
 
@@ -25,7 +25,7 @@ def test_ascii():
 
 
 def test_detect():
-    testfiles = glob.glob('src/tests/testdata/*/*.txt')
+    testfiles = glob.glob(os.path.join('src','tests','testdata','*','*.txt'))
     for testfile in testfiles:
         if testfile.replace("\\", "/") in SKIP_LIST:
             continue
@@ -40,7 +40,7 @@ def test_detect():
 
 def test_detector():
     detector = cchardet.UniversalDetector()
-    with open("src/tests/samples/wikipediaJa_One_Thousand_and_One_Nights_SJIS.txt", 'rb') as f:
+    with open(os.path.join("src','tests','samples','wikipediaJa_One_Thousand_and_One_Nights_SJIS.txt", 'rb')) as f:
         line = f.readline()
         while line:
             detector.feed(line)
@@ -66,7 +66,7 @@ def test_github_issue_20():
 
 
 def test_decode():
-    testfiles = glob.glob('src/tests/testdata/*/*.txt')
+    testfiles = glob.glob(os.path.join('src','tests','testdata','*','*.txt'))
     for testfile in testfiles:
         if testfile.replace("\\", "/") in SKIP_LIST_02:
             continue
